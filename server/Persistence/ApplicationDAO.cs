0626841d2927930935946f363e7f9c4a7e05c4cb
niekg <niekg@mail.uc.edu>
2018-04-04 14:14:57 -0400

    trying to figure out connection strings

--- server/Persistence/ApplicationDAO.cs
+++ server/Persistence/ApplicationDAO.cs
@@ -14,14 +14,14 @@ namespace server.Persistence
 {
     public class ApplicationDAO
     {
-        private static string connectionString = "User ID = manager;Password=manager;Server=localhost;Port=5432;Database=ResumeManager.Dev;Integrated Security=true; Pooling=true;";
-        private static string connStr = "Host=localhost;Database=ResumeManager;Username=manager;Password=manager";
-
-        public static IDbConnection Connect(){
-            return new NpgsqlConnection(connectionString);
+        public static IDbConnection Connection{
+            get{
+                return new NpgsqlConnection("postgresql://Server=localhost;Port=5432;Database=ResumeManager;User Id=manager;Password=manager;");
+            }
+            
         }
         public static Application ReadApplication(int id){
-            using(var db = Connect()){
+            using(var db = Connection){
                 string queryStr = "SELECT * FROM public.Application WHERE ApplicationID = @Id";
                 db.Open();
                 return db.Query<Application>(queryStr, new { Id = id }).FirstOrDefault();
@@ -29,7 +29,7 @@ namespace server.Persistence
         }
 
         public static IEnumerable<Application> ReadAllApplications(){
-            using(var db = Connect()){
+            using(var db = Connection){
                 string queryStr = "SELECT * FROM public.Applcation";
                 db.Open();
                 return db.Query<Application>(queryStr);
@@ -37,7 +37,7 @@ namespace server.Persistence
         }
 
         public static void CreateApplication(Application application){
-            using(var db = Connect()){
+            using(var db = Connection){
                 string queryStr = "INSERT INTO public.Application (FirstName, LastName, Email, PositionId, ResumePath) VALUES(@FirstName, @LastName, @Email, @PositionId, @ResumePath)";
                 db.Open();
                 db.Execute(queryStr, application);
@@ -45,7 +45,7 @@ namespace server.Persistence
         }
 
         public static void DeleteApplication(int id){
-            using(var db = Connect()){
+            using(var db = Connection){
                 string queryStr = "DELETE * FROM public.Application WHERE ApplicationId = @ Id";
                 db.Open();
                 db.Execute(queryStr, new { Id = id });
